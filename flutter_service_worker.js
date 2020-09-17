797f39150264311e2de052bd3485f34fe2d6325a
Jordan ALCARAZ <jordan.alcaraz.pro@gmail.com>
2020-09-17 18:10:54 +0200

    try to fix build

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -5,7 +5,7 @@ const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "index.html": "4cea2d0fab1ebdd8759a0b1c3d9c79d1",
 "/": "4cea2d0fab1ebdd8759a0b1c3d9c79d1",
-"main.dart.js": "25875635024eaa9100b60a3bf47f2d17",
+"main.dart.js": "0aa2e18a04d823f0912b143bd9affcaa",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
