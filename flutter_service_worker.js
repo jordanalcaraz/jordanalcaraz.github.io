025e61fa3a239ca6064dff6ca7fd72d692b5d289
Jordan ALCARAZ <jordan.alcaraz.pro@gmail.com>
2020-09-18 11:25:29 +0200

    responsive experience

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -5,7 +5,7 @@ const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "index.html": "4cea2d0fab1ebdd8759a0b1c3d9c79d1",
 "/": "4cea2d0fab1ebdd8759a0b1c3d9c79d1",
-"main.dart.js": "150e68a19d496ddd316b30a4b69bc4e4",
+"main.dart.js": "df61b744a42fa520b87d24a4740927db",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
